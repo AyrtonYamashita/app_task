020f23f5c882669209bfe4ac1ae3fff4ee13c733
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2023-05-23 17:41:55 -0400

    resolvendo a funcao fetchTask()

--- public/main.js
+++ public/main.js
@@ -15,63 +15,68 @@ function addTask() {
             fetchTask();
             taskInput.value = '';
         })
-            .catch((error) => {
-                console.error(error);
-            })
         tasks.push(task);
         input.value = "";
     }
 }
 
-// Função para mostrar as tarefas na lista
-function renderTask(){
-    const taskList = document.getElementById("taskList");
-    taskList.innerHTML = "";
+// // Função para mostrar as tarefas na lista
+// function renderTask(){
+//     const taskList = document.getElementById("taskList");
+//     taskList.innerHTML = "";
 
-    tasks.forEach((task) => {
-        const taskItem = document.createElement("div");
-        if (task.check){
-            taskItem.classList.add("complete-task");
-        }else {
-            taskItem.classList.add("task");
-        }
-        taskItem.innerHTML = `
-        <button onclick="checkList(${task.id})" class="task-check">✔</button>
-        <span class="task-text">${task.text}</span>
-        <button onclick="orderTask(${task.id})" class="order-task">⬇</button>
-        <button onclick="confirmDeleteTask(${task.id})" class="task-delete">🗑️</button>`;
-        taskList.appendChild(taskItem);
-    });
+//     tasks.forEach((task) => {
+//         const taskItem = document.createElement("div");
+//         if (task.check){
+//             taskItem.classList.add("complete-task");
+//         }else {
+//             taskItem.classList.add("task");
+//         }
+//         taskItem.innerHTML = `
+//         <button onclick="checkList(${task.id})" class="task-check">✔</button>
+//         <span class="task-text">${task.text}</span>
+//         <button onclick="orderTask(${task.id})" class="order-task">⬇</button>
+//         <button onclick="confirmDeleteTask(${task.id})" class="task-delete">🗑️</button>`;
+//         taskList.appendChild(taskItem);
+//     });
+// }
+
+async function loadTask() {
+    const tasks = await fetchTask();
+        tasks.map((tarefa) => {
+            const taskItem = document.createElement("div");
+            if (tarefa.check){
+                taskItem.classList.add("complete-task");
+            }else {
+                taskItem.classList.add("task");
+            }
+            taskItem.innerHTML = `
+            <button onclick="checkList(${tarefa.id})" class="task-check">✔</button>
+            <span class="task-text">${tarefa.text}</span>
+            <button onclick="orderTask(${tarefa.id})" class="order-task">⬇</button>
+            <button onclick="confirmDeleteTask(${tarefa.id})" class="task-delete">🗑️</button>`;
+            taskList.appendChild(taskItem);
+        })
+        
 }
 
 // Função para buscar as tarefas do servidor
-function fetchTask() {
-    fetch("/tasks").then((res) => {
-        res.json().then((dados) => {
-            dados.map((tarefa) => {
-                const taskItem = document.createElement("div");
-                if (tarefa.check){
-                    taskItem.classList.add("complete-task");
-                }else {
-                    taskItem.classList.add("task");
-                }
-                taskItem.innerHTML = `
-                <button onclick="checkList(${tarefa.id})" class="task-check">✔</button>
-                <span class="task-text">${tarefa.text}</span>
-                <button onclick="orderTask(${tarefa.id})" class="order-task">⬇</button>
-                <button onclick="confirmDeleteTask(${tarefa.id})" class="task-delete">🗑️</button>`;
-                taskList.appendChild(taskItem);
-            });
-        })
-
-    })
+async function fetchTask() {
+    const response = await fetch('/tasks');
+    const tasks = await response.json();
+    return tasks;
 }
 
+// Função para marcar a tarefa como concluida
+function checkList(tarefaId) {
+    filtrado = tasks.filter((tarefa) => tarefa.id === tarefaId);
+    // const task = filtrado[0];
+    // task.check = !task.check;
+    console.log(filtrado);
+}
 // Função para ordenar uma tarefa
-function orderTask(tarefaId) {
-    currentIndex = tasks.findIndex((tarefa) => tarefa.id === tarefaId);
-    console.log(currentIndex);
-
+function orderTask(taskId) {
+    currentIndex = tasks.findIndex((task) => task.id === taskId);
     const newIndex = currentIndex + 1;
     if (newIndex < tasks.length) {
         const currentTask = tasks.splice(currentIndex, 1);
@@ -80,27 +85,18 @@ function orderTask(tarefaId) {
     }
 }
 
-// Função para marcar a tarefa como concluida
-function checkList(taskId) {
-    filtrado = tasks.filter((task) => task.id === taskId);
-    const task = filtrado[0];
-    task.check = !task.check;
-    fetchTask();
-}
-
 // Função para exibir a mensagem de confirmação antes de excluir uma task
 function confirmDeleteTask(taskId) {
     if (confirm("Tem certeza que deseja remover esta tarefa?")) {
-        removeTask(taskId);
+        deleteTask();
     }
 }
 
-// Função para excluir uma tarefa
-function removeTask(taskId) {
-    tasks = tasks.filter((task) => task.id !== taskId);
-    deleteTask();
-
-}
+// // Função para excluir uma tarefa
+// function removeTask(taskId) {
+//     tasks = tasks.filter((task) => task.id !== taskId);
+//     console.log(tasks);
+// }
 
 async function saveTask(task) {
     try {
@@ -120,18 +116,9 @@ async function saveTask(task) {
 }
 
 
-async function deleteTask(taskId) {
-    try {
-        const response = await fetch(`/tasks/${taskId}`, {
-            method: 'DELETE'
-        });
-        if (!response.ok) {
-            throw new Error('Erro ao deletar a tarefa');
-        }
-        const deletedTask = await response.json();
-        return deletedTask;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+async function deleteTask(task) {
+    fetch(`/tasks/${task}`, {
+        method: 'DELETE'
+    })
 }
-fetchTask();
+loadTask();
