c1f639db4a515dbaf1f2ba49f1fbf834a398db5b
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2023-05-26 14:50:33 -0400

    ultimos ajustes

--- public/main.js
+++ public/main.js
@@ -1,6 +1,33 @@
 // Criado um array para armazenar as tarefas
 let tasks = [];
 
+// Função para buscar as tarefas do servidor
+async function fetchTask() {
+    const response = await fetch('/tasks');
+    const tasks = await response.json();
+    return tasks;
+}
+// Função para carregar as tarefas
+async function loadTask() {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+    const tasks = await fetchTask();
+    tasks.map((tarefa) => {
+        const taskItem = document.createElement("div");
+        if (tarefa.check) {
+            taskItem.classList.add("complete-task");
+        } else {
+            taskItem.classList.add("task");
+        }
+        taskItem.innerHTML = `
+            <button onclick="checkList(${tarefa.id})" class="task-check">✔</button>
+            <span class="task-text">${tarefa.text}</span>
+            <button onclick="orderTask(${tarefa.id})" class="order-task">⬇</button>
+            <button onclick="confirmDeleteTask(${tarefa.id})" class="task-delete">🗑️</button>`;
+        taskList.appendChild(taskItem);
+    })
+
+}
 // Função para adicionar uma nova tarefa
 function addTask() {
     const input = document.getElementById("taskInput");
@@ -12,92 +39,67 @@ function addTask() {
             check: false
         };
         saveTask(task).then(() => {
-            fetchTask();
+            loadTask();
             taskInput.value = '';
         })
         tasks.push(task);
         input.value = "";
     }
 }
-
-// // Função para mostrar as tarefas na lista
-// function renderTask(){
-//     const taskList = document.getElementById("taskList");
-//     taskList.innerHTML = "";
-
-//     tasks.forEach((task) => {
-//         const taskItem = document.createElement("div");
-//         if (task.check){
-//             taskItem.classList.add("complete-task");
-//         }else {
-//             taskItem.classList.add("task");
-//         }
-//         taskItem.innerHTML = `
-//         <button onclick="checkList(${task.id})" class="task-check">✔</button>
-//         <span class="task-text">${task.text}</span>
-//         <button onclick="orderTask(${task.id})" class="order-task">⬇</button>
-//         <button onclick="confirmDeleteTask(${task.id})" class="task-delete">🗑️</button>`;
-//         taskList.appendChild(taskItem);
-//     });
-// }
-
-async function loadTask() {
-    const tasks = await fetchTask();
-        tasks.map((tarefa) => {
-            const taskItem = document.createElement("div");
-            if (tarefa.check){
-                taskItem.classList.add("complete-task");
-            }else {
-                taskItem.classList.add("task");
-            }
-            taskItem.innerHTML = `
-            <button onclick="checkList(${tarefa.id})" class="task-check">✔</button>
-            <span class="task-text">${tarefa.text}</span>
-            <button onclick="orderTask(${tarefa.id})" class="order-task">⬇</button>
-            <button onclick="confirmDeleteTask(${tarefa.id})" class="task-delete">🗑️</button>`;
-            taskList.appendChild(taskItem);
-        })
-        
-}
-
-// Função para buscar as tarefas do servidor
-async function fetchTask() {
-    const response = await fetch('/tasks');
-    const tasks = await response.json();
-    return tasks;
-}
-
 // Função para marcar a tarefa como concluida
 function checkList(tarefaId) {
-    filtrado = tasks.filter((tarefa) => tarefa.id === tarefaId);
-    // const task = filtrado[0];
-    // task.check = !task.check;
-    console.log(filtrado);
+    try {
+        fetch(`/tasks/check/${tarefaId}`, {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error('Falha ao atualizar a tarefa');
+            }
+            loadTask();
+        });
+    } catch (error) {
+        console.error(error);
+    }
 }
 // Função para ordenar uma tarefa
-function orderTask(taskId) {
-    currentIndex = tasks.findIndex((task) => task.id === taskId);
-    const newIndex = currentIndex + 1;
-    if (newIndex < tasks.length) {
-        const currentTask = tasks.splice(currentIndex, 1);
-        tasks.splice(newIndex, 0, currentTask[0]);
-        fetchTask();
+function orderTask(tarefaId) {
+    try {
+        fetch(`/tasks/order/${tarefaId}`, {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response =>{
+            if(!response.ok){
+                throw new Error ('Falha ao ordenar a tarefa');
+            }
+            loadTask();
+        });
+    }catch (error){
+        console.error(error);
     }
-}
-
+} 
 // Função para exibir a mensagem de confirmação antes de excluir uma task
-function confirmDeleteTask(taskId) {
+function confirmDeleteTask() {
     if (confirm("Tem certeza que deseja remover esta tarefa?")) {
-        deleteTask();
+        removeTask();
     }
 }
-
-// // Função para excluir uma tarefa
-// function removeTask(taskId) {
-//     tasks = tasks.filter((task) => task.id !== taskId);
-//     console.log(tasks);
-// }
-
+// Função para excluir uma tarefa
+function removeTask(tarefaId) {
+    fetch(`/tasks/delete/${tarefaId}`, {
+        method: 'DELETE'
+    }).then(response => {
+        if(!response.ok){
+            throw new Error ('Falha ao excluir a tarefa');
+        }
+        loadTask();
+    })
+}
+// Função para salvar uma tarefa
 async function saveTask(task) {
     try {
         const response = await fetch('/tasks', {
@@ -113,12 +115,5 @@ async function saveTask(task) {
     } catch (error) {
         throw new Error(error.message);
     }
-}
-
-
-async function deleteTask(task) {
-    fetch(`/tasks/${task}`, {
-        method: 'DELETE'
-    })
 }
 loadTask();
--- server.js
+++ server.js
@@ -5,89 +5,139 @@ const app = express();
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-
+// Rota padrão que carrega a pagina inicial da minha aplicação
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname + '/public'});
+    res.sendFile('index.html', { root: __dirname + '/public' });
 });
 
+// Rota carrega as informações dentro do arquivo tasks.json
 app.get('/tasks', (req, res) => {
     fs.readFile('tasks.json', 'utf8', (err, data) => {
-        if(err){
+        if (err) {
             console.log(err);
-            return res.status(500).json({ error: 'Erro ao ler as tarefas!'});
+            return res.status(500).json({ error: 'Erro ao ler as tarefas!' });
         }
         let tasks = [];
         if (data) {
-          try {
-            tasks = JSON.parse(data);
-          } catch (error) {
-            console.error(error);
-            return res.status(500).json({ error: 'Erro ao ler o arquivo JSON' });
-          }
+            try {
+                tasks = JSON.parse(data);
+            } catch (error) {
+                console.error(error);
+                return res.status(500).json({ error: 'Erro ao ler o arquivo JSON' });
+            }
         }
-    
+
         res.json(tasks);
     });
 });
 
+// Rota que envia informações para dentro do arquivo tasks.json
 app.post('/tasks', (req, res) => {
     fs.readFile('tasks.json', 'utf8', (err, data) => {
-        if(err){
+        if (err) {
             console.log(err);
-            return res.status(500).json({error: 'Erro ao ler as tarefas!'});
+            return res.status(500).json({ error: 'Erro ao ler as tarefas!' });
         }
 
         let tasks = [];
         if (data) {
-          try {
-            tasks = JSON.parse(data);
-          } catch (error) {
-            console.error(error);
-            return res.status(500).json({ error: 'Failed to parse tasks JSON' });
-          }
+            try {
+                tasks = JSON.parse(data);
+            } catch (error) {
+                console.error(error);
+                return res.status(500).json({ error: 'Failed to parse tasks JSON' });
+            }
         }
         const newTask = req.body;
         tasks.push(newTask);
         fs.writeFile('tasks.json', JSON.stringify(tasks), 'utf8', (err) => {
-            if(err){
+            if (err) {
                 console.error(err);
-                return res.status(500).json({ error: 'Erro ao salvar as tarefas!'});
+                return res.status(500).json({ error: 'Erro ao salvar as tarefas!' });
             }
             res.json(newTask);
         });
     });
 });
 
-app.delete('/tasks/:id', (req, res) => {
-    fs.readFile('tasks.json', 'utf8', (err, data) => {
-        if(err){
-            console.log(err);
-            return res.status(500).json({error: 'Erro ao ler as tarefas!'});
+// Rota que deleta as informações dentro do arquivo tasks.json
+app.delete('/tasks/delete/:id', (req, res) => {
+    fs.readFile('tasks.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo JSON', err);
+            return;
         }
-
         let tasks = [];
-        if(data){
-            try {
-                tasks = JSON.parse(data);
-            } catch (error){
-                console.error(error);
-                return res.status(500).json({error: 'Erro ao ler o arquivo JSON'});  
-            }
+        tasks = JSON.parse(data);
+        const tarefaId = parseInt(req.params.id);
+        const tarefaIndex = tasks.findIndex(task => task.id !== tarefaId);
+        tasks.splice(tarefaIndex, 1);
+            fs.writeFile('tasks.json', JSON.stringify(tasks), 'utf-8', (err) => {
+                if (err) {
+                    console.error('Erro ao atualizar o arquivo JSON:', err);
+                    res.status(500).send('Erro ao atualizar a tarefa!');
+                    return;
+                }
+                res.send('Tarefa atualizada com sucesso!');
+            });
+    });
+})
+
+// Rota que sobrescreve os objetos dentro do arquivo tasks.json
+app.post('/tasks/check/:id', (req, res) => {
+    fs.readFile('tasks.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo JSON', err);
+            return;
         }
-        const taskId = req.params.id;
-        const taskIndex = tasks.findIndex((task) => task.id === taskId);
-        if(taskIndex === -1){
-            return res.status(404).json({error: 'Tarefa não encontrada!'});
+        let tasks = [];
+        tasks = JSON.parse(data);
+        const tarefaId = parseInt(req.params.id);
+        const tarefaIndex = tasks.findIndex(task => task.id === tarefaId);
+        if (tarefaIndex !== -1) {
+            tasks[tarefaIndex].check = !tasks[tarefaIndex].check;
+            fs.writeFile('tasks.json', JSON.stringify(tasks), 'utf-8', (err) => {
+                if (err) {
+                    console.error('Erro ao atualizar o arquivo JSON:', err);
+                    res.status(500).send('Erro ao atualizar a tarefa!');
+                    return;
+                }
+                res.send('Tarefa atualizada com sucesso!');
+            });
+        }else{
+            res.status(404).send('Tarefa não encontrada')
         }
-        tasks.splice(taskIndex, 1);
-        fs.writeFile('tasks.json', JSON.stringify(tasks), 'utf8', (err) => {
-            if (err){
-                console.error(err);
-                return res.status(500).json({error: 'Erro ao deletar a tarefa'});
-            }
-            res.json({concluido: true});
-        })
-    })
+    });
+})
+
+// Rota que ordena a lista de tarefas
+app.post('/tasks/order/:id', (req, res) => {
+    fs.readFile('tasks.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo JSON', err);
+            return;
+        }
+        let tasks = [];
+        tasks = JSON.parse(data);
+        const tarefaId = parseInt(req.params.id);
+        const tarefaIndex = tasks.findIndex(task => task.id === tarefaId);
+        const newIndex = tarefaIndex + 1;
+        if(newIndex < tasks.length){
+            const currentTask = tasks.splice(tarefaIndex, 1);
+            tasks.splice(newIndex, 0, currentTask[0]);
+            fs.writeFile('tasks.json', JSON.stringify(tasks), 'utf-8', (err) => {
+                if (err) {
+                    console.error('Erro ao atualizar o arquivo JSON:', err);
+                    res.status(500).send('Erro ao ordenar a tarefa!');
+                    return;
+                }
+                res.send('Tarefa ordenada com sucesso!');
+            });
+        }else{
+            res.status(404).send('Tarefa não encontrada');
+        }
+    });
+
 })
 
 app.listen(3000, () => {
